6ee7adf87b70a231bbca5ec313bd165bc02d9adf
vitorcostagarcia2006@gmail.com <vitorcostagarcia2006@gmail.com>
2025-07-25 11:44:36 -0300

    blueprint of some ideas presented in chapter 1 and 3 todo

---
+++ 1-introduction/tictactoe.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Environment dynamics
+Win -> R = 1 Winn must be rewarded
+Loss -> R = -1 Loss must be penalized 
+Draw -> R = 0 Draw must not be the same as a loss so is a game where he cant win anymore, it prioritizes a draw instead of a loss
+Non terminal state -> R = 0;
+*/
+
+int main(){
+
+}
